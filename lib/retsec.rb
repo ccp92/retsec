d40c734d739861884c46e454a6abe15d86962c9e
Christopher Parsons <mail@christopherparsons.me>
2018-02-12 21:31:25 +0000

    Even method complete A method has been added that satisfies the tests for all even number strings.

--- lib/retsec.rb
+++ lib/retsec.rb
@@ -2,6 +2,28 @@ require 'pry'
 class Retsec
 
   def self.convert(input)
-    "retsec"
+    array = input.split('') # input string converted into an array
+    chars = input.length # variable holding the length of the string
+    newarray = []
+    i = 0 # counter for while loops
+    if input.length.even? # here I am acting on the even numbered strings
+      section_1_end = (chars/2) - 1 # specifying the end point of the 1st half
+      section_2_begin = section_1_end + 1 # and the beginning point of the second
+      while section_2_begin < array.length
+        # this while loop is grabbing the second half of the string and adding
+        # it to a new array
+        newarray << array[section_2_begin]
+        section_2_begin = section_2_begin + 1
+      end
+      while i <= section_1_end
+        # this while loop is adding the first half of the string to the end of
+        # the new array
+        newarray << array[i]
+        i = i + 1
+      end
+      newarray.join
+    else
+      -1
+    end
   end
 end
